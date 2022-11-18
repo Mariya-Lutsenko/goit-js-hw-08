1a97c6ae30ba1d2fb7bebd976f22e173369b9536
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2022-11-18 16:17:42 +0200

    simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,30 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = creatImagesMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+
+
+function creatImagesMarkup (galleryItems) {
+    const markup = galleryItems.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `;
+    }).join('');
+    return markup;
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+	captionDelay: 250,
+    captionsData: 'alt',
+})
