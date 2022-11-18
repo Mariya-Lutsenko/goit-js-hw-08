1afd3de15054110a9742243c6741aced50cf3989
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2022-11-18 17:01:48 +0200

    ex2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
 
+const  throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+player.on('timeupdate', throttle (timeUpdatePlayer, 1000));
+
+
+function  timeUpdatePlayer (event) {
+localStorage.getItem('videoplayer-current-time', event.seconds);
+}
