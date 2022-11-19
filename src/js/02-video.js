a47a57b9917d61bd5c3891e0e5bfd6b00122c1ba
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2022-11-20 00:41:20 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,17 +4,27 @@ const  throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const TIME_KEY = 'videoplayer-current-time';
 
 
+timeCheck ();
 
 const onPlay = function ({seconds}) {
-    localStorage.setItem('videoplayer-current-time', seconds);
+    localStorage.setItem(TIME_KEY, seconds);
     };
     
 player.on('timeupdate',  throttle(onPlay, 1000));
 
 player
-.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.setCurrentTime(localStorage.getItem(TIME_KEY))
 .catch(function (error) {
     console.error(error)
 });
+
+
+function timeCheck () {
+    if (localStorage.getItem(TIME_KEY) === null) {
+        return localStorage.setItem (TIME_KEY, 0)
+    }
+    player.setCurrentTime(localStorage.getItem(TIME_KEY));
+}
